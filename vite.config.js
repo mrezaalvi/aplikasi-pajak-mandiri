7dfa51532bb7533dfee8b9bfe3421c437022a541
Muhammad Reza Alvi <mrezaalvi@gmail.com>
2023-06-23 00:34:11 +0700

    Updated gitignore file

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
                 'resources/js/app.js',
                 'resources/css/filament.css',
             ],
+            ssr: 'resources/js/ssr.js',
             refresh: true,
         }),
     ],
